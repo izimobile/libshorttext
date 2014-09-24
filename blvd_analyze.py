0963fc90343e15f07ac8e54a20449e67c11dd063
Zachary J. Rollyson <zach@izimobile.com>
2014-09-24 13:13:12 -0400

    Through the trail of indices we go to get the best word.

--- blvd_analyze.py
+++ blvd_analyze.py
@@ -1,4 +1,5 @@
 import blvd_text
+import re
 
 from libshorttext.analyzer import *
 from libshorttext.classifier import *
@@ -12,7 +13,26 @@ def analyze(text):
     prediction_res = predict_single_text(str(text), analyzer.model)
     decvals = prediction_res.decvals
     features, weights, labels = analyzer.model.get_weight(str(text))
-    print 'something'
 
+    idx = decvals.index(max(decvals))
+    label = labels[idx]
+    label_weights = []
+    # probably maps or something clever
+    for weight in weights:
+        label_weights.append(weight[idx])
+    feature_idx = label_weights.index(max(label_weights))
+    feature = features[feature_idx]
+    try:
+        token_idx = tokens.index(feature)
+    except ValueError:
+        for token in tokens:
+            if feature[0] in token:
+                token_idx = tokens.index(token)
+
+    word_idx = indices[token_idx]
+    word = word_list[word_idx]
+
+    print label
+    print word
 
 analyze("OMG biggest sale ever #SaleYourSoul no furreal 98% off don't miss this sale you bastard.")
