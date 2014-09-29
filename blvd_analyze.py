3645369daac3f4dcd135161b2908807f17ff50f6
zachizi <zach@izimobile.com>
2014-09-29 13:47:15 -0400

    Wrap blvd_analyze in a pointless class & add ZeroRPC server to it.

--- blvd_analyze.py
+++ blvd_analyze.py
@@ -1,5 +1,5 @@
 import blvd_text
-import re
+import json
 
 from libshorttext.analyzer import *
 from libshorttext.classifier import *
@@ -7,32 +7,41 @@ from libshorttext.classifier import *
 analyzer = Analyzer('outputs/test1.model')
 
 
-def analyze(text):
-    tokens, indices, word_list = blvd_text.tokenize_with_indices(text)
-    text = ' '.join(tokens)
-    prediction_res = predict_single_text(str(text), analyzer.model)
-    decvals = prediction_res.decvals
-    features, weights, labels = analyzer.model.get_weight(str(text))
+class BlvdAnalyzer():
 
-    idx = decvals.index(max(decvals))
-    label = labels[idx]
-    label_weights = []
-    # probably maps or something clever
-    for weight in weights:
-        label_weights.append(weight[idx])
-    feature_idx = label_weights.index(max(label_weights))
-    feature = features[feature_idx]
-    try:
-        token_idx = tokens.index(feature)
-    except ValueError:
-        for token in tokens:
-            if feature[0] in token:
-                token_idx = tokens.index(token)
+    def __init__(self):
+        self.is_currently_useless = True
 
-    word_idx = indices[token_idx]
-    word = word_list[word_idx]
+    @staticmethod
+    def run(text):
+        tokens, indices, word_list = blvd_text.tokenize_with_indices(text)
+        text = ' '.join(tokens)
+        prediction_res = predict_single_text(str(text), analyzer.model)
+        decvals = prediction_res.decvals
+        features, weights, labels = analyzer.model.get_weight(str(text))
 
-    print label
-    print word
+        idx = decvals.index(max(decvals))
+        label = labels[idx]
+        label_weights = []
+        # probably maps or something clever
+        for weight in weights:
+            label_weights.append(weight[idx])
+        feature_idx = label_weights.index(max(label_weights))
+        feature = features[feature_idx]
+        try:
+            token_idx = tokens.index(feature)
+        except ValueError:
+            for token in tokens:
+                if feature[0] in token:
+                    token_idx = tokens.index(token)
 
-analyze("OMG biggest sale ever #SaleYourSoul no furreal 98% off don't miss this sale you bastard.")
+        word_idx = indices[token_idx]
+        word = word_list[word_idx]
+
+        return json.dumps({'relevant_word': word, 'tag': label})
+
+import zerorpc
+
+s = zerorpc.Server(BlvdAnalyzer())
+s.bind('tcp://0.0.0.0:4241')
+s.run()
