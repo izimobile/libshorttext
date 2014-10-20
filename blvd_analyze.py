f2e0ad55afe611b1f9fb8340909ffb880a7ca317
zachizi <zach@izimobile.com>
2014-10-20 09:44:48 -0400

    Check for label_weights before working on them.

--- blvd_analyze.py
+++ blvd_analyze.py
@@ -31,19 +31,22 @@ class BlvdAnalyzer():
         # probably maps or something clever
         for weight in weights:
             label_weights.append(weight[idx])
-        feature_idx = label_weights.index(max(label_weights))
-        feature = features[feature_idx]
-        try:
-            token_idx = tokens.index(feature)
-        except ValueError:
-            for token in tokens:
-                if feature[0] in token:
-                    token_idx = tokens.index(token)
+        if label_weights:
+            feature_idx = label_weights.index(max(label_weights))
+            feature = features[feature_idx]
+            try:
+                token_idx = tokens.index(feature)
+            except ValueError:
+                for token in tokens:
+                    if feature[0] in token:
+                        token_idx = tokens.index(token)
 
-        word_idx = indices[token_idx]
-        word = word_list[word_idx]
+            word_idx = indices[token_idx]
+            word = word_list[word_idx]
 
-        return json.dumps({'relWord': word, 'tag': label})
+            return json.dumps({'relWord': word, 'tag': label})
+        else:
+            return None
 
 s = zerorpc.Server(BlvdAnalyzer())
 s.bind('tcp://0.0.0.0:4241')
