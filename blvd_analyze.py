bc70247441f343dd28fb572c726dc3100fe43aa0
Zachary J Rollyson <zach@izimobile.com>
2015-10-26 09:24:00 -0400

    Update tagger: 15.10.26

--- blvd_analyze.py
+++ blvd_analyze.py
@@ -6,7 +6,7 @@ import json
 from libshorttext.analyzer import *
 from libshorttext.classifier import *
 
-analyzer = Analyzer('outputs/15.8.19.model')
+analyzer = Analyzer('outputs/15.10.26.model')
 
 import zerorpc
 
